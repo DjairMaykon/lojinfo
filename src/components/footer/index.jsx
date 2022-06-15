57eefe9be33b8af9e080716ae82c0fd9fd327460
Pedro Amaral <pedro.brandao1230@gmail.com>
2022-06-15 14:21:33 -0300

    Rotas do menu, responsivo e CSS atualizados

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,19 +1,46 @@
 import './style.css'
+import { Link } from 'react-router-dom'
+import twitterLogo from '../../assets/twitterLogo.svg'
+import facebookLogo from '../../assets/facebookLogo.svg'
+import instagramLogo from '../../assets/instagramLogo.svg'
+import Logo from '../../assets/Logo.svg'
 
-export function Footer(){
-  return <footer>
-    <ul className='navMenuFooter'>
-      <li>Início</li>
-      <li>Produtos</li>
-      <li>Anunciar</li>
-      <li>Carrinho</li>
-    </ul>
-    <img className='logoLojinhaFooter' src="./imgs/Logo" alt="Logo da Lojinha" />
-    <div className='socicalMediaLogosFooter'>
-      <img src="" alt="Logo do Facebook" />
-      <img src="" alt="Logo do Twitter" />
-      <img src="" alt="logo do Instagram" />
-    </div>
-    <p className='footerMessage'>Feito pela Infojr UFBA com Figma, React e muito 💚</p>
-  </footer>
+export function Footer() {
+  return (
+    <footer>
+      <section className="logoMenuSocialContainer">
+        <ul className="navMenuFooter">
+          <li>
+            <Link to="/" className="colorNavMenuFooter">
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link to="/product" className="colorNavMenuFooter">
+              Produtos
+            </Link>
+          </li>
+          <li>
+            <Link to="/product" className="colorNavMenuFooter">
+              Anunciar
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="colorNavMenuFooter">
+              Carrinho
+            </Link>
+          </li>
+        </ul>
+        <img className="logoLojinhaFooter" src={Logo} alt="Logo da Lojinha" />
+        <div className="socicalMediaLogosFooter">
+          <img src={facebookLogo} alt="Logo do Facebook" />
+          <img src={twitterLogo} alt="Logo do Twitter" />
+          <img src={instagramLogo} alt="logo do Instagram" />
+        </div>
+      </section>
+      <p className="footerMessage">
+        Feito pela Infojr UFBA com Figma, React e muito 💚
+      </p>
+    </footer>
+  )
 }
