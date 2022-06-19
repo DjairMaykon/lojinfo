775dbee9be06c04e7a8756a3a77d89697fa853fc
Pedro Amaral <pedro.brandao1230@gmail.com>
2022-06-19 16:21:49 -0300

    Categorias adicionadas a página home e reponsivo

--- src/components/categoriasHome/index.jsx
+++ src/components/categoriasHome/index.jsx
@@ -2,16 +2,16 @@ import './style.css'
 import BotaoComprarCategorias from '../../assets/botaoComprarCategorias.svg'
 export function CategoriasHome(props) {
   return (
-    <section className="cardContainer">
+    <article className="cardContainer">
       <img
         className="imgCategories"
         src={props.img}
         alt="Imagem referente a categoria do produto."
       />
       <div className="insideCardContainer">
-        <p className="textCategories">{props.text}</p>
+        <h1 className="textCategories">{props.text}</h1>
         <div className="butonSeeMore">
-          <p className="textSeeMoreCategories">Ver mais</p>
+          <h2 className="textSeeMoreCategories">Ver mais</h2>
           <img
             className="seeMoreCategories"
             src={BotaoComprarCategorias}
@@ -19,6 +19,6 @@ export function CategoriasHome(props) {
           />
         </div>
       </div>
-    </section>
+    </article>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
+import { CategoriasHome } from '../../components/categoriasHome'
 import { Footer } from '../../components/footer'
 import { Header } from '../../components/header'
 import { HeroHome } from '../../components/heroHome/HeroHome'
 import { SectionProducts } from '../../components/sectionProducts'
-
+import BolsaCategorias from '../../assets/bolsaCategorias.svg'
+import CamisaCategorias from '../../assets/camisaCategorias.svg'
+import TenisCategorias from '../../assets/tenisCategorias.svg'
 import './style.css'
 
 export function Home() {
@@ -11,6 +14,11 @@ export function Home() {
       <Header />
       <main id="mainHome">
         <HeroHome />
+        <section className="categotiasContainerHome">
+          <CategoriasHome text="Camisas" img={CamisaCategorias} />
+          <CategoriasHome text="Tênis" img={TenisCategorias} />
+          <CategoriasHome text="Bolsas" img={BolsaCategorias} />
+        </section>
         <SectionProducts />
       </main>
       <Footer />
