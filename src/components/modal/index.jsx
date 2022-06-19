bf565acd2b3a43f9f490b9dfeedccd4146b961d9
Pedro Amaral <pedro.brandao1230@gmail.com>
2022-06-19 12:33:18 -0300

    Estrutura do modal

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,46 @@
+import './style.css'
+
+export function Modal() {
+  return (
+    <Modal>
+      <img src="" alt="" />
+      <p></p>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h4></h4>
+          <h4></h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3></h3>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <h1></h1>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h5></h5>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h5></h5>
+          </div>
+        </div>
+        <button></button>
+      </div>
+    </Modal>
+  )
+}
