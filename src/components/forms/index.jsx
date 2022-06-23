a94702bad02479939b299ffe675aafd783514480
djairmaykon <djairmaykon@gmail.com>
2022-06-23 10:41:54 -0300

    feat: formulario de cadastrar/editar produtos se comunicando com a api

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,42 +1,117 @@
 import './style.css'
 import defaultImageProduct from '../../assets/defaultImageProduct.svg'
+import { useState } from 'react'
+
 export function Forms(props) {
+  const categories = ['Vestuário', 'Calçados', 'Acessórios']
+  const subcategories = ['Camisas', 'Tênis', 'Bolsas']
+  const [productUrl, setProductUrl] = useState(
+    props.product ? props.product.url : null
+  )
+  const [productTitle, setProductTitle] = useState(
+    props.product ? props.product.title : null
+  )
+  const [productDescription, setProductDescription] = useState(
+    props.product ? props.product.description : null
+  )
+  const [productBrand, setProductBrand] = useState(
+    props.product ? props.product.brand : null
+  )
+  const [productColor, setProductColor] = useState(
+    props.product ? props.product.color : null
+  )
+  const [productCategory, setProductCategory] = useState(
+    props.product ? props.product.category : categories[0]
+  )
+  const [productSubcategory, setProductSubcategory] = useState(
+    props.product ? props.product.subcategory : subcategories[0]
+  )
+  const [productPrice, setProductPrice] = useState(
+    props.product ? props.product.price : null
+  )
+  const [formInvalid, setFormInvalid] = useState({
+    url: false,
+    title: false,
+    description: false,
+    brand: false,
+    color: false,
+    category: false,
+    subcategory: false,
+    price: false,
+  })
+
+  function handleSendForm() {
+    setFormInvalid({
+      url: !productUrl || productUrl == '',
+      title: !productTitle || productTitle == '',
+      description:
+        !productDescription ||
+        productDescription == '' ||
+        productDescription.length > 300,
+      brand: !productBrand || productBrand == '',
+      color: !productColor || productColor == '',
+      category: !productCategory || productCategory == '',
+      subcategory: !productSubcategory || productSubcategory == '',
+      price: !productPrice || productPrice == '' || isNaN(productPrice),
+    })
+    if (Object.entries(formInvalid).some(e => e[1])) return
+
+    props.onSendForm({
+      url: productUrl,
+      title: productTitle,
+      description: productDescription,
+      brand: productBrand,
+      color: productColor,
+      category: productCategory,
+      subcategory: productSubcategory,
+      price: productPrice,
+    })
+  }
+
   return (
-    <form id="formProduto">
+    <form onSubmit={e => e.preventDefault()} id="formProduto">
       <div className="imgForms">
         <h3 className="titleFormsH3">Preview da imagem</h3>
         <div className="innerConteinerImgForms">
           <img
             className="defaultImageProduct"
-            src={defaultImageProduct}
+            src={productUrl ? productUrl : defaultImageProduct}
             alt="Modelo para adicionar a imagem do produto."
           />
         </div>
       </div>
+
       <div className="formsContainerLessImg">
         <div className="inputUrlForms">
           <h4 className="titleFormsH4">Url da imagem</h4>
           <input
-            className="inputTextForms"
+            className={`inputTextForms ${formInvalid['url'] ? 'invalid' : ''}`}
             type="text"
-            placeholder={props.urlImg}
+            onChange={e => setProductUrl(e.target.value)}
+            value={productUrl ?? ''}
           />
         </div>
         <div className="inputTitleForms">
           <h4 className="titleFormsH4">Título</h4>
           <input
-            className="inputTextForms"
+            className={`inputTextForms ${
+              formInvalid['title'] ? 'invalid' : ''
+            }`}
             type="text"
-            placeholder={props.title}
+            onChange={e => setProductTitle(e.target.value)}
+            value={productTitle ?? ''}
           />
         </div>
         <div className="inputDescriptionForms">
           <div>
             <h4 className="titleFormsH4">Descrição do produto</h4>
             <input
-              className="inputTextForms"
+              className={`inputTextForms ${
+                formInvalid['description'] ? 'invalid' : ''
+              }`}
               type="text"
-              placeholder={props.description}
+              onChange={e => setProductDescription(e.target.value)}
+              value={productDescription ?? ''}
             />
           </div>
           <div className="descriptionFormsWarningMessage">
@@ -47,17 +122,23 @@ export function Forms(props) {
           <div className="inputBrandForms">
             <h4 className="titleFormsH4">Marca</h4>
             <input
-              className="inputTextForms"
+              className={`inputTextForms ${
+                formInvalid['brand'] ? 'invalid' : ''
+              }`}
               type="text"
-              placeholder={props.brand}
+              onChange={e => setProductBrand(e.target.value)}
+              value={productBrand ?? ''}
             />
           </div>
           <div className="inputColorForms">
             <h4 className="titleFormsH4">Cor</h4>
             <input
-              className="inputTextForms"
+              className={`inputTextForms ${
+                formInvalid['color'] ? 'invalid' : ''
+              }`}
               type="text"
-              placeholder={props.color}
+              onChange={e => setProductColor(e.target.value)}
+              value={productColor ?? ''}
             />
           </div>
         </div>
@@ -65,31 +146,50 @@ export function Forms(props) {
         <div className="inputCategoriesForms">
           <div className="innerDivCategories">
             <h4 className="titleFormsH4">Categoria</h4>
-            <select name="" className="inputSelectForms">
-              <option value="">Vestuário</option>
-              <option value="">Calçados</option>
-              <option value="">Acessórios</option>
+            <select
+              onChange={e => setProductCategory(e.target.value)}
+              value={productCategory ?? ''}
+              name=""
+              className="inputSelectForms"
+            >
+              {categories.map((category, i) => (
+                <option key={i} value={category}>
+                  {category}
+                </option>
+              ))}
             </select>
           </div>
           <div className="innerDivCategories">
             <h4 className="titleFormsH4">Subcategoria</h4>
-            <select name="" className="inputSelectForms">
-              <option value="">Camisas</option>
-              <option value="">Tênis</option>
-              <option value="">Bolsas</option>
+            <select
+              value={productSubcategory ?? ''}
+              onChange={e => setProductSubcategory(e.target.value)}
+              name=""
+              className="inputSelectForms"
+            >
+              {subcategories.map((subcategory, i) => (
+                <option key={i} value={subcategory}>
+                  {subcategory}
+                </option>
+              ))}
             </select>
           </div>
         </div>
         <div className="inputPriceForms">
           <h4 className="titleFormsH4">Preço</h4>
           <input
-            className="inputTextForms"
+            className={`inputTextForms ${
+              formInvalid['price'] ? 'invalid' : ''
+            }`}
             type="text"
-            placeholder={props.price}
+            onChange={e => setProductPrice(e.target.value)}
+            value={productPrice ?? ''}
           />
         </div>
         <div className="alignButtonContainer">
-          <button className="buttonForms"> {props.text}</button>
+          <button onClick={() => handleSendForm()} className="buttonForms">
+            {(props.product ? 'Editar' : 'Cadastrar') + ' produto'}
+          </button>
         </div>
       </div>
     </form>
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -2,8 +2,69 @@ import './style.css'
 import { Footer } from '../../components/footer'
 import { Header } from '../../components/header'
 import { Forms } from '../../components/forms'
+import { useParams } from 'react-router-dom'
+import api from '../../utils/api'
+import { useEffect, useState } from 'react'
 
 export function Product() {
+  const [product, setProduct] = useState()
+  const { id } = useParams()
+  useEffect(() => {
+    if (id) {
+      api.get(`produtos/${id}`).then(response => {
+        if (response.data.response.length) {
+          const produto = response.data.response[0]
+          setProduct({
+            id: produto.id,
+            price: produto.preco,
+            url: produto.url,
+            title: produto.titulo,
+            description: produto.descricao,
+            brand: produto.marca,
+            color: produto.cor,
+            category: produto.categoria,
+            subcategory: produto.subcategoria,
+          })
+        }
+      })
+    }
+  }, [id])
+
+  function onSendForm(product) {
+    if (id) {
+      console.log(product)
+      api
+        .put(`produtos/${id}`, {
+          preco: product.price,
+          url: product.url,
+          titulo: product.title,
+          descricao: product.description,
+          marca: product.brand,
+          cor: product.color,
+          categoria: product.category,
+          subcategoria: product.subcategory,
+        })
+        .then(() => {
+          alert('Produto editado com sucesso')
+        })
+    } else {
+      api
+        .post('produtos', {
+          preco: product.price,
+          url: product.url,
+          titulo: product.title,
+          descricao: product.description,
+          marca: product.brand,
+          cor: product.color,
+          categoria: product.category,
+          subcategoria: product.subcategory,
+        })
+        .then(() => {
+          alert('Produto adicionado com sucesso')
+        })
+    }
+  }
+
   return (
     <>
       <Header />
@@ -12,7 +73,9 @@ export function Product() {
           <h1 className="titleFormsH1">Venha vender com a gente!</h1>
           <p className="titleFormsP">Cadastre seu produto e venda na Lojinha</p>
         </div>
-        <Forms text={'Cadastrar produto'} />
+        {(!id || (id && product)) && (
+          <Forms product={product} onSendForm={onSendForm} />
+        )}
       </main>
 
       <Footer />
